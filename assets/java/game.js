53eb317a5c7b4fe123f634758be54dc0fb9d1793
Johnathan vargas <j.vargas2319@gmail.com>
2019-03-22 10:30:19 -0700

    added to files

--- assets/java/game.js
+++ assets/java/game.js
@@ -58,21 +58,72 @@ when game resets generate new random number*/
 
 $(document).ready(function () {
 
-    var playerScore = 0;
-    var wins = 0;
-    var losses = 0;
-    var randomNum = 0;
-    var gems = [];
+    var playerScore = 0; //on reset change to 0
+    var wins = 0; // on reset keep storing players wins
+    var losses = 0;// on reset keepl storing player losses
+    var randomNum = 0;// on reset change to new random number
+
     function genNumber(min, max) {
 
         return Math.floor(Math.random() * (max - min + 1) + min);
 
     }
 
-    console.log(genNumber(19, 120));
+    randomNum = genNumber(19, 120);
+    $(".randomNum").html(randomNum);// update html on reset
+    var gem1 = genNumber(1, 12);// update to new random number on reset
+    var gem2 = genNumber(1, 12);// update to new random number on reset
+    var gem3 = genNumber(1, 12);// update to new random number on reset
+    var gem4 = genNumber(1, 12);// update to new random number on reset
 
+    function reset() {
+        gem1 = genNumber(1, 12);// update to new random number on reset
+        gem2 = genNumber(1, 12);// update to new random number on reset
+        gem3 = genNumber(1, 12);// update to new random number on reset
+        gem4 = genNumber(1, 12)
 
+        playerScore = 0; //on reset change to 0
+        $('.playerScoreText').html(playerScore);
+        randomNum = genNumber(19, 120);
+        $(".randomNum").html(randomNum);// update html on reset
+      
+    }
 
+    $('#gem1').click(function () {
+        playerScore = playerScore + gem1;
+        $('.playerScoreText').html(playerScore);
+        compScore();
+    });
+
+    $('#gem2').click(function () {
+        playerScore = playerScore + gem2;
+        $('.playerScoreText').html(playerScore);
+        compScore();
+    });
+    $('#gem3').click(function () {
+        playerScore = playerScore + gem3;
+        $('.playerScoreText').html(playerScore);
+        compScore();
+    });
+    $('#gem4').click(function () {
+        playerScore = playerScore + gem4;
+        $('.playerScoreText').html(playerScore);
+        compScore();
+    });
+
+    var compScore = function () {
+        if (playerScore === randomNum) {
+            alert("You da Champ!!!");
+            wins++;
+            $(".win").html(wins);
+            reset();
+        } else if (playerScore > randomNum) {
+            alert("You lost");
+            losses++;
+            $(".losse").html(losses);
+            reset();
+        }
+    }
 
 
 });
